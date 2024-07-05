c984d1cf5907033b51620f1b4e5e8a271e857dd7
oseiascosta <oseiasc2@gmail.com>
2024-07-04 23:10:39 -0300

    feat: included Datepicker

--- app/docsDetails/newSolicitation.tsx
+++ app/docsDetails/newSolicitation.tsx
@@ -14,6 +14,9 @@ import useGetCompanys from '../hook/useGetCompanys';
 import { MaterialIcons } from '@expo/vector-icons';
 import RadioItem from '@ant-design/react-native/lib/radio/RadioItem';
 import { Ionicons } from '@expo/vector-icons';
+import DatePicker from '@ant-design/react-native/lib/date-picker';
+import Provider from '@ant-design/react-native/lib/provider';
+import List from '@ant-design/react-native/lib/list'
 
 export default function NewSolicitation() {
   const [other, setOther] = useState({
@@ -25,6 +28,7 @@ export default function NewSolicitation() {
   const { data: companys } = useGetCompanys();
   const [selectCompany, setSelectCompany] = useState(false);
   const [companySelected, setCompanySelected] = useState({ id: '', name: '' });
+  const [expiration, setExpiration] = useState();
 
   return (
     <View style={{ backgroundColor: '#fff', paddingTop: 20, flex: 1 }}>
@@ -46,7 +50,7 @@ export default function NewSolicitation() {
             </TouchableOpacity>
           </View>
           <View style={style.body}>
-            <View style={{paddingTop: 10}}>
+            <View style={{ paddingTop: 10 }}>
               {companys?.map((company: any) => {
                 return (
                   <RadioItem
@@ -59,11 +63,14 @@ export default function NewSolicitation() {
                     value={company.id}
                     children={
                       <View style={{ marginTop: 4 }}>
-                        <Text style={{
+                        <Text
+                          style={{
                             fontFamily: 'Lato_400Regular',
                             fontSize: 18,
                             color: companySelected.name === company.name ? '#1677ff' : '#5D5B5B',
-                        }}>{company.name}</Text>
+                          }}>
+                          {company.name}
+                        </Text>
                       </View>
                     }
                     left={true}
@@ -84,10 +91,9 @@ export default function NewSolicitation() {
           marginVertical: 5,
           marginHorizontal: 20,
           flexDirection: 'row',
-          justifyContent: 'space-between'
+          justifyContent: 'space-between',
         }}
-        onPress={() => setSelectCompany(true)}
-      >
+        onPress={() => setSelectCompany(true)}>
         <Text
           numberOfLines={1}
           style={{
@@ -122,7 +128,51 @@ export default function NewSolicitation() {
         }}
       />
       <TextInputCustom label="Descrição" text="" />
-      <TextInputCustom label="Prazo" text="" />
+      <Provider>
+        <List>
+        <DatePicker
+          value={expiration}
+          mode="date"
+          title="Selecione"
+          locale={{
+            okText: 'Ok',
+            dismissText: 'Cancelar',
+            DatePickerLocale: {
+              day: '',
+              year: '',
+              month: '',
+              am: '',
+              pm: '',
+              hour: '',
+              minute: '',
+            },
+          }}
+          defaultDate={new Date()}
+          minDate={new Date()}
+          maxDate={new Date(2026, 11, 3)}
+          onChange={(e: any) => setExpiration(e)}
+          format="YYYY-MM-DD">
+          <List.Item
+            style={{
+              width: '100%',
+              borderColor: '#DADADA',
+              borderWidth: 1,
+              height: 47,
+              padding: 10,
+              borderRadius: 5,
+              marginVertical: 5,
+              marginHorizontal: 20,
+              flexDirection: 'row',
+              // justifyContent: 'space-between',
+              // fontFamily: 'Lato_400Regular',
+              // fontSize: 18,
+            }}
+          ><Text>Selecione a Data</Text>
+          <MaterialIcons name="date-range" size={24} color="black" />
+          </List.Item>
+        </DatePicker>
+        </List>
+      </Provider>
       <TextInputCustom label="Solicitante" text="" />
     </View>
   );
